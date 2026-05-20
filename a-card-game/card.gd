4a766f723a6a92339bae555485db706889cc47b6
Lazerlemur <145408883+lazerlemur@users.noreply.github.com>
2026-05-20 16:44:40 -0700

    Added basic card scene and JSON card storage
    The card scene is a generic card which can be instanced and given a set of specific traits using a JSON file. A new JSON file will be created for each specific card we add, and when an instance of a card is created, it will be assigned to one of the JSONs which will give it its set of instance variables such as type, name, health, abilities and texture. The card is a 2d scene, which will be rendered in the 3d gameplay scene using a subviewport.

---
+++ a-card-game/card.gd
@@ -0,0 +1,31 @@
+extends Node2D
+var currentSleeve = "none"
+var cardData = {}
+#change this filepath to choose specific card JSON, to be changed to recieve signal when card instance is created
+var dataFilePath = "res://cards/attributes/card1.json"
+var cardName
+var cardType
+var cardHealth
+var cardPoints
+var cardAbilities
+func _ready():
+	cardData = load_json_file(dataFilePath)
+	#please ignore this fucking dogshit placeholder texture I drew in like 5 minutes
+	$cardTexture.texture = load("res://cards/textures/" + cardData.get("texture"))
+	cardName = cardData.get("name")
+	cardType = cardData.get("type")
+	cardHealth = cardData.get("health")
+	cardPoints = cardData.get("points")
+	cardAbilities = cardData.get("abilities")
+#converts selected 
+func load_json_file(filePath : String):
+	if FileAccess.file_exists(filePath):
+		var dataFile = FileAccess.open(filePath, FileAccess.READ)
+		var parsedResult = JSON.parse_string(dataFile.get_as_text())
+		if parsedResult is Dictionary:
+			return parsedResult
+		else:
+			print("file not formatted correctly >:3")
+	else: 
+		print("file doesn't exist 3:")
+		
